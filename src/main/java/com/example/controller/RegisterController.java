58b99a266215eb5e1f10bb2972162847bcc1512f
takara.miyazaki <takara.miyazaki@rakus.co.jp>
2019-05-27 15:19:50 +0900

    15km:コントローラー作成

---
+++ src/main/java/com/example/controller/RegisterController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/15km")
+public class RegisterController {
+	
+	@Autowired
+	private ServletContext application;
+	
+	@RequestMapping("")
+	public String index() {
+		return "buy-goods";
+	}
+	
+	@RequestMapping("/receive")
+	public String calcSum(int goods1, int goods2, int goods3) {
+		int price = goods1 + goods2 + goods3;
+		int priceInculeTax = (int) (price*1.08);
+		
+		application.setAttribute("price", price);
+		application.setAttribute("priceInculeTax", priceInculeTax);
+		
+		return "total-price";
+	}
+
+}
