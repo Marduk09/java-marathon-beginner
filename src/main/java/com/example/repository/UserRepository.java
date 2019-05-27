a57707f44e3c9fd70d9d7d5fcf9b30458f5797e7
takara.miyazaki <takara.miyazaki@rakus.co.jp>
2019-05-27 16:37:56 +0900

    20km:インプット、アウトプットのビューを作成。レポジトリのSQLを修正

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -11,6 +12,7 @@ import com.example.domain.User;
 @Repository
 public class UserRepository {
 	
+	@Autowired
 	private NamedParameterJdbcTemplate template;
 	
 	private static final RowMapper<User> USER_ROW_MAPPER
@@ -24,9 +26,9 @@ public class UserRepository {
 	};
 	
 	public User load(Integer id) {
-		String sql = "SELECT name, age, address FROM info "
-					+ "WHERE id=:id";
-		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		String sql = "SELECT id, name, age, address FROM info "
+					+ " WHERE id=:id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id); 
 		
 		User user = template.queryForObject(sql, param, USER_ROW_MAPPER);
 		
