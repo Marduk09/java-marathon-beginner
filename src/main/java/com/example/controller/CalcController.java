74a9cae72a7388f62c61e1ddcbb20ad16e544f4c
takara.miyazaki <takara.miyazaki@rakus.co.jp>
2019-05-27 14:51:36 +0900

    10km:コントローラー作成

---
+++ src/main/java/com/example/controller/CalcController.java
@@ -0,0 +1,35 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/10m")
+public class CalcController {
+	
+	
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping("")
+	public String index() {
+		return "input-value";
+	}
+	
+	@RequestMapping("/receive")
+	public String calcReceive(Integer num1, Integer num2) {
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
+		session.setAttribute("result", num1*num2);
+		return "output-value";
+	}
+	
+	@RequestMapping("/to-value2")
+	public String toValue2() {
+		return "output-to-value2";
+	}
+
+}
