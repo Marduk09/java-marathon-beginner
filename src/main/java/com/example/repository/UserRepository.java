e5a22056fc38b3e1853d4687577e6c4e9b86db52
takara.miyazaki <takara.miyazaki@rakus.co.jp>
2019-05-27 16:07:40 +0900

    20km:リリポジトリ作成、DBに合わせてUserドメインにidフィールド追加

---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,36 @@
+package com.example.repository;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.User;
+
+@Repository
+public class UserRepository {
+	
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<User> USER_ROW_MAPPER
+		= (rs, i) -> {
+			User user = new User();
+			user.setId(rs.getInt("id"));
+			user.setName(rs.getString("name"));
+			user.setAge(rs.getInt("age"));
+			user.setAddress(rs.getString("address"));
+			return user;
+	};
+	
+	public User load(Integer id) {
+		String sql = "SELECT name, age, address FROM info "
+					+ "WHERE id=:id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		
+		User user = template.queryForObject(sql, param, USER_ROW_MAPPER);
+		
+		return user;
+	}
+
+}
